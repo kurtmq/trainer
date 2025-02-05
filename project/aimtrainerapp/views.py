94c649c84bb15936496a0b1af9a89e65a656bdd3
harrypawer <168436569+kurtmq@users.noreply.github.com>
2025-02-05 11:57:13 +0800

    running aimtrainer app

--- project/aimtrainerapp/views.py
+++ project/aimtrainerapp/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'aimtrainerapp/home.html')
+
+def game(request):
+    return render(request, 'aimtrainerapp/game.html')
